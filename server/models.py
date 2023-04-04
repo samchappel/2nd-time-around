0121880f55aa07a24c77abb6f34b9b16dcf8e801
sam chappel <m.samchappel@gmail.com>
2023-04-04 15:55:55 -0700

    added user model with validations

--- server/models.py
+++ server/models.py
@@ -1,1 +1,74 @@
 
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy.ext.hybrid import hybrid_property
+from sqlalchemy.orm import validates
+
+from app import bcrypt
+
+db = SQLAlchemy()
+
+class User(db.Model, SerializerMixin):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    email = db.Column(db.String)
+    _password_hash = db.Column(db.String)
+    admin = db.Column(db.String, default=False)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+     @hybrid_property
+    def password_hash(self):
+        raise Exception('Password hashes may not be viewed.')
+
+    @password_hash.setter
+    def password_hash(self, password):
+        password_hash = bcrypt.generate_password_hash(
+            password.encode('utf-8'))
+        self._password_hash = password_hash.decode('utf-8')
+
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(
+            self._password_hash, password.encode('utf-8'))
+
+    @validates('email')
+    def validate_email(self, key, email):
+        users = User.query.all()
+        emails = [user.email for user in user]
+        if not email:
+            raise ValueError('Email must be provided')
+        elif email in emails:
+            raise ValueError('This email is already registered to an account - please log in.')
+        elif not re.search('@', email):
+            raise ValueError('Must be a valid email')
+        return email
+
+    @validates('password')
+    def validate_password(self, key, password):
+        if len(password) < 8:
+            raise ValueError('Password must be at least 8 characters long.')
+        elif not re.search('[!@#$%^&*]', password):
+            raise ValueError('Password must contain at least one special character.')
+        return password
+        
+    @validates('first_name')
+    def validate_first_name(self, key, value):
+        if not value:
+            raise ValueError('First name must be provided')
+        return value
+    
+    def __repr__(self):
+        return f'CLIMBER: ID: {self.id}, Name {self.first_name}, Username: {self.username}, Admin: {self.admin}'
+
+    def __repr__(self):
+        return f'USER: ID: {self.id}, Name {self.name}, Email: {self.email}, Admin: {self.admin}'
+
+
+
+# username
+# email
+# password_hash
+# created_at
+# updated_at
